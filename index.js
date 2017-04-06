632438b1ee684f68a6f05863ac6fd929c46df754
Sultan Arziyev <sultan99@mail.ru>
2017-04-06 15:36:42 +0600

    FIX. Fixed first bug.

---
+++ index.js
@@ -0,0 +1,17 @@
+const Module = require(`module`)
+const join = require(`path`).join
+const normalize = require(`path`).normalize
+const paths = require(`../../package.json`).path
+
+const _require = Module.prototype.require
+Module.prototype.require = function(path) {
+	if (path.indexOf(`$`) >= 0) {
+		let root = normalize(`${__dirname}/../..`)
+		for (let dirname in paths) {
+			path = path.replace(dirname, join(root, paths[dirname]))
+		}
+	}
+	return _require.call(this, path)
+}
+
+module.exports = Module.prototype.require
