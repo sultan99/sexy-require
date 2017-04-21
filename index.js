7705c441c42b9e00636625e853bfd85730304046
Sultan Arziyev <sultan99@mail.ru>
2017-04-21 10:36:55 +0600

    Rolled back to old version

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const _require = Module.prototype.require
 
 Module.prototype.require = function(path) {
 	if (path.indexOf(`$`) >= 0) {
+		let root = normalize(`${__dirname}/../..`)
 		for (let dirname in paths) {
-			path = path.replace(dirname, paths[dirname])
+			path = path.replace(dirname, join(root, paths[dirname]))
 		}
 	}
-	const root = normalize(`${__dirname}/../..`)
-	return _require.call(this, join(root, path))
+	return _require.call(this, path)
 }
 
 module.exports = Module.prototype.require
